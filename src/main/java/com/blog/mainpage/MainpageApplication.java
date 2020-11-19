7f94d9467244fa0af5b0be0235823d8b8ae41c09
柳军 <2519754650@qq.com>
2020-11-19 12:00:27 +0800

    test

---
+++ src/main/java/com/blog/mainpage/MainpageApplication.java
@@ -0,0 +1,13 @@
+package com.blog.mainpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainpageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainpageApplication.class, args);
+    }
+
+}
